1cf8692378f5c70dec87a0fb9931d6b7b55f0791
skywater61020 <158059068+skywater61020@users.noreply.github.com>
2024-01-29 22:45:31 +0700

    Update skywahub.lua

--- skywahub.lua
+++ skywahub.lua
@@ -1,4 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/skywater61020/skywahub.lua/main/skywahub.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
